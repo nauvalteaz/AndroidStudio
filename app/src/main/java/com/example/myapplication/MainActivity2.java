c5d609007f8b604a39313af7a99a6adbc266a4b2
nauvalteaz <nauvalteaz@gmail.com>
2022-10-02 23:00:27 +0700

    go

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
-    Button btnBio;
+    ImageView btnBio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
